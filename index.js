53827f791e963c5e5ac693986b403fde1cbe3fce
mason white <masondwhite1@gmail.com>
2022-09-08 19:03:06 -0500

    commit tree 6b5ebce228c86c0f5c732c026747b76eedc98a50

---
+++ index.js
@@ -0,0 +1,41 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+
+function sayHiToHeadphonedRoommate(string){
+  if (string === string.toLowerCase()){
+    return "I can\'t hear you!"
+  } else if  
+    (string === string.toUpperCase())
+    return "YES INDEED!"
+}
+
+
+/*
+function sayHiToHeadphonedRoommate(string) {
+  switch (string) {
+    case (string === string.toLowerCase())
+      : return "I can\'t hear you!"
+      break
+    case (string === string.toUpperCase())
+      : return "YES INDEED!"
+      break
+    case (string === "Let\'s have dinner together!")
+      : return "I would love to!"
+      break
+  }
+}
+*/
